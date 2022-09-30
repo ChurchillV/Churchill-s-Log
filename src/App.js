532fe8046c2d8a41c0881b680efe1f2a3ed52845
ChurchillV <104873546+ChurchillV@users.noreply.github.com>
2022-09-30 21:40:56 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react'
+import TodoList from './TodoList'
+import { v4 as uuid } from 'uuid'
+import './index.css'
+
+
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  function clearCompleteTodo() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
+  
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuid(), name: name, complete: false }]
+    })
+    //console.log(name)
+    todoNameRef.current.value = null
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <main>
+    <div className='mainArea'>
+      <TodoList todos = {todos} className="TodoListItems" toggleTodo={toggleTodo}/>
+      <input ref = {todoNameRef} type="text" className="InputField" placeholder='Enter your Task'/> <br/>
+      <button onClick = {handleAddTodo} className="addButton">Add Item</button>
+      <button className='clearButton' onClick= {clearCompleteTodo}> Clear Complete</button>
+      <div className="progressTracker">{todos.filter(todo => !todo.complete).length} left to do</div>
     </div>
-  );
+    </main>
+    
+  )
+   
 }
 
 export default App;
